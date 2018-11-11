8bd48dec472a645ed7e3354391c22140d6533e44
cl <d.atenaustauschl@gmx.at>
2018-11-11 13:31:51 +0100

    import new ones

---
+++ 110-install-sound-v3-cl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+#Sound
+sudo pacman -S pulseaudio --noconfirm --needed
+sudo pacman -S pulseaudio-alsa --noconfirm --needed
+sudo pacman -S pavucontrol  --noconfirm --needed
+sudo pacman -S alsa-utils alsa-plugins alsa-lib alsa-firmware --noconfirm --needed
+sudo pacman -S gstreamer --noconfirm --needed
+sudo pacman -S gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly gst-libav --noconfirm --needed
+sudo pacman -S volumeicon --noconfirm --needed
+sudo pacman -S playerctl --noconfirm --needed
+
+echo "################################################################"
+echo "#########   sound software software installed   ################"
+echo "################################################################"
